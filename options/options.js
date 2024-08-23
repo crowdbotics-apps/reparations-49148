9640372cb490af6513616117e116dde1c653e1a6
Crowdbotics <team@crowdbotics.com>
2024-08-23 01:35:31 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "reparations-49148",
+  url: "https://reparations-49148.botics.co",
+  api: "https://reparations-49148.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
